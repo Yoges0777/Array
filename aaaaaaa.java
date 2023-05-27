b5f55f51db1839544e01cde3e9961613810504e5
Yoges0777 <121220208+Yoges0777@users.noreply.github.com>
2023-05-27 11:09:00 +0530

    Add files via upload

---
+++ aaaaaaa.java
@@ -0,0 +1,31 @@
+import java.util.Iterator;
+
+public class aaaaaaa 
+{
+
+	public static void main(String[] args) 
+	{
+		int []a= {1,2,3,4,5};
+		for (int i = 0; i < a.length; i++) 
+		{
+			int cnt=0;
+			for (int j = 0; j < a.length; j++) 
+			{
+				if (a[i]==a[j]) 
+				{
+					cnt++;
+				}
+			}
+			if (cnt==0) 
+			{
+				for (int j = 0; j < a.length; j++)
+				{
+					System.out.println();
+				}
+			}
+		}
+	}
+
+
+}	
+
